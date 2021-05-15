046de05de86ae6a00b478e15231ea5b224d9c4c2
bubovaleon <bubova.leon@gmx.de>
2021-05-15 16:58:14 +0200

    fix lint errors

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     style: {
         postcss: {
+            // eslint-disable-next-line global-require,import/no-extraneous-dependencies
             plugins: [require('tailwindcss'), require('autoprefixer')],
         },
     },
